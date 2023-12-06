2381accd6e6f452e82db39aa0becc11f508bd9f0
Nand2nand <nand28041994@gmail.com>
2023-12-06 16:31:49 +0530

    console-log the request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log('req made')
+    console.log(req.url,req.method)
 });
 server.listen(3000,'localhost',()=>{
     console.log('listening for requests on port 3000')
