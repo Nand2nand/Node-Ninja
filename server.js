21ba8857a72f63e6024b9ec8fde4b181dc673ba9
Nand2nand <nand28041994@gmail.com>
2023-12-06 15:33:57 +0530

    initial

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
     console.log('req made')
-})
+});
 server.listen(3000,'localhost',()=>{
     console.log('listening for requests on port 3000')
-})
+});
